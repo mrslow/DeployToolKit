15dab5c079fe2a5b3b3c44a7cbf638d9b0a26a55
mrslow <animal2k@gmail.com>
2016-10-28 13:36:25 +0300

    Move package files to folder

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
@@ -8,11 +8,12 @@ with open('LICENSE') as f:
 
 setup(
     name='deploytoolkit',
-    version='0.1.0',
+    version='0.1',
     description='Tool for easy deploying.',
     long_description=readme,
     author='Anton Shchetikhin',
     author_email='animal2k@gmail.com',
+    packages = ['deploytoolkit'],
     url='https://github.com/mrslow/DeployToolKit',
     license=license
 )
